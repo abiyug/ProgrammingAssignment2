045506d907402d8d34335bd4305f51e3226d4b0e
abiyug <abiyu@antexusolutions.com>
2015-06-21 16:48:47 -0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i  <- NULL
+        set  <- function(y){
+                x <<- y
+                i <<- NULL 
+        }
+        get  <- function() x
+        setinvr  <- function(inverse) i  <<- inverse
+        getinvr  <- function() i
+        list(set= set, get = get, 
+             setinvr = setinvr, 
+             getinvr = getinvr)
+        
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i  <- x$getinvr()
+        if (!is.null(i)){
+                message("Fetching cached data")
+                return(i)
+        }
+        data  <- x$get()
+        i  <- solve(data, ...)
+        x$setinvr(i)
+        i
 }
